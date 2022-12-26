1febd6d2ac4dc2f556eda65748bbd0129b3d62f0
Harshal Sai Kanumuri <121031084+HarshalSaiKanumuri@users.noreply.github.com>
2022-12-26 16:38:58 +0000

    Update HC-SR04 Dist.ino

--- HC-SR04 Dist.ino
+++ HC-SR04 Dist.ino
@@ -1,35 +1,53 @@
-#define trig 4
-#define echo 3
+#define TRIG_PIN 4  // Trig pin of HC-SR04 connected to digital pin 10
+#define ECHO_PIN 3  // Echo pin of HC-SR04 connected to digital pin 11
+#define NUM_SAMPLES 5  // Number of samples to take for noise averaging
 
-void setup(){
-  // put your setup code here, to run once:
+void setup() {
+  // Set Trig and Echo pins as output and input, respectively
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  // Initialize serial communication for debugging purposes
   Serial.begin(9600);
-  pinMode(trig,OUTPUT);
-  pinMode(echo,INPUT);
 }
 
-void loop(){
-  Distance();
+void loop() {
+  long distance = getDistance();  // Get distance to object in cm
 
+  // Print distance to serial monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);  // Delay for half a second before taking another reading
 }
 
-void Distance(){
-  digitalWrite(trig,LOW);
-  delay(0.002);
-  digitalWrite(trig,HIGH);
-  delay(0.01);
-  digitalWrite(trig,LOW);
-  delay(0.002);
+// Function to get distance to object in cm using noise averaging
+long getDistance() {
+  long sum = 0;  // Sum of samples
 
-  float duration = pulseIn(echo,HIGH);
-  float time = duration / 2;
-
-  // Assuming 20 degrees Celcius
-  float distance = 0.0343 * time;
-  if (distance >= 4 && distance <= 400)
-  {
-    Serial.print("The distance is ");
-    Serial.print(distance);
-    Serial.print("\n");
+  // Take multiple samples and sum them
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    sum += getDistanceSample();
   }
+
+  // Return average of samples
+  return sum / NUM_SAMPLES;
+}
+
+// Function to get a single distance sample in cm
+long getDistanceSample() {
+  digitalWrite(TRIG_PIN, LOW);  // Set Trig pin low
+  delayMicroseconds(2);  // Delay for 2 microseconds
+  digitalWrite(TRIG_PIN, HIGH);  // Set Trig pin high
+  delayMicroseconds(10);  // Delay for 10 microseconds
+  digitalWrite(TRIG_PIN, LOW);  // Set Trig pin low
+
+  // Measure the time it takes for the Echo pin to go high (pulse width)
+  long pulseWidth = pulseIn(ECHO_PIN, HIGH);
+
+  // Calculate distance to object in cm using pulse width and speed of sound
+  long distance = pulseWidth / 58.2;
+
+  return distance;
 }
